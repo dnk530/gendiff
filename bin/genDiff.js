03669b60cd8ba1f1b5a27b88cb37061a01662940
O. Marchenko <alex.mar4enko@gmail.com>
2021-12-16 11:04:42 -0800

    add: file comparison and output

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,9 +1,10 @@
-#!/usr/bin/env node --experimental-json-modules --no-warnings
+#!/usr/bin/env node
 import genDiff from '../src/index.js'
 import { Command } from 'commander/esm.mjs';
-import packageConfig from '../package.json';
 import path from 'path';
 import { readFileSync } from 'fs';
+
+const packageConfig = JSON.parse(readFileSync(path.resolve('./package.json')))
 
 const program = new Command();
 program
@@ -13,10 +14,10 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
-    const abspath1 = path.resolve(filepath1);
-    const abspath2 = path.resolve(filepath2);
-    const file1 = JSON.parse(readFileSync(abspath1, 'UTF-8'));
-    const file2 = JSON.parse(readFileSync(abspath2, 'UTF-8'));
+    const absPath1 = path.resolve(filepath1);
+    const absPath2 = path.resolve(filepath2);
+    const file1 = JSON.parse(readFileSync(absPath1, 'UTF-8'));
+    const file2 = JSON.parse(readFileSync(absPath2, 'UTF-8'));
     genDiff(file1, file2)
   })
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
+import _ from 'lodash';
+
 export default (file1, file2) => {
   console.log(file1);
   console.log(file2);
-  return null;
+
+  const keys = _.sortBy(_.uniq([...Object.keys(file1), ...Object.keys(file2)]));
+  console.log(keys);
+
+  const result = keys.map(key => {
+    if (_.has(file1, key) && _.has(file2, key)) {
+      return (
+        file1[key] === file2[key]
+        ? `    ${key}: ${file1[key]}`
+        : `  - ${key}: ${file1[key]}
+  + ${key}: ${file2[key]}`
+      )
+    };
+    if (!_.has(file1, key) && _.has(file2, key)) {
+      return (`  + ${key}: ${file2[key]}`);
+    };
+    if (_.has(file1, key) && !_.has(file2, key)) {
+      return (`  - ${key}: ${file1[key]}`);
+    };
+  });
+
+console.log(['{', ...result, '}'].join('\n'));
+
+return result;
 };
