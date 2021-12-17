ea1e4d2782e07bc33b28b74a7ad75a3294f354c7
O. Marchenko <alex.mar4enko@gmail.com>
2021-12-16 16:16:07 -0800

    fix: imports in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import path from 'path';
+import * as path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { test, expect } from '@jest/globals';
@@ -12,7 +12,6 @@ test('gendiff ', () => {
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename); // ?
   const file1 = JSON.parse(fs.readFileSync(getFixturePath('file1.json'), 'utf-8'));
   const file2 = JSON.parse(fs.readFileSync(getFixturePath('file2.json'), 'utf-8'));
-  console.log(file1, file2);
   const expectedOutput = fs.readFileSync(getFixturePath('expected_file.txt'), 'utf-8');
   expect(genDiff(file1, file2)).toEqual(expectedOutput);
 });
