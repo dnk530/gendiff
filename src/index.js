4a12122932e1267dbf77372b28a9f72fea0e5952
O. Marchenko <alex.mar4enko@gmail.com>
2021-12-23 12:28:18 -0800

    refactor: diff and formatter

--- src/index.js
+++ src/index.js
@@ -7,23 +7,28 @@ export default (filepath1, filepath2) => {
 
   const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
 
-  const diff = keys.map((key) => {
+  const diff = keys.flatMap((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
     if (!_.has(obj1, key)) {
-      return (`  + ${key}: ${value2}`);
+      return [['+', [key, value2]]];
     }
     if (!_.has(obj2, key)) {
-      return (`  - ${key}: ${value1}`);
+      return [['-', [key, value1]]];
     }
     return (
       value1 === value2
-        ? `    ${key}: ${value1}`
-        : `  - ${key}: ${value1}
-  + ${key}: ${value2}`
+        ? [[' ', [key, value1]]]
+        : [['-', [key, value1]], ['+', [key, value2]]]
     );
   });
 
-  const result = (['{', ...diff, '}'].join('\n'));
+  const format = (data) => {
+    const INDENT = '  ';
+    const lines = data.map(([sign, [key, value]]) => `${INDENT}${sign} ${key}: ${value}`);
+    return ['{', ...lines, '}'].join('\n');
+  };
+  const result = format(diff);
+
   return result;
 };
