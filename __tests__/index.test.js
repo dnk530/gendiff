d667053732a6e3b2c29f3bc463f910528c50116c
O. Marchenko <alex.mar4enko@gmail.com>
2021-12-23 19:03:01 -0800

    add: nested structures test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,29 +8,35 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const expectedOutput = fs.readFileSync(getFixturePath('/simple/output.txt'), 'utf-8');
+const expectedOutput = fs.readFileSync(getFixturePath('/plain/output.txt'), 'utf-8');
 const output = fs.readFileSync(getFixturePath('output.txt'), 'utf-8');
 
 test('gendiff json', () => {
-  const filepath1 = getFixturePath('/simple/file1.json');
-  const filepath2 = getFixturePath('/simple/file2.json');
+  const filepath1 = getFixturePath('/plain/file1.json');
+  const filepath2 = getFixturePath('/plain/file2.json');
   expect(genDiff(filepath1, filepath2)).toEqual(expectedOutput);
 });
 
 test('gendiff yaml', () => {
-  const filepath1 = getFixturePath('/simple/file1.yml');
-  const filepath2 = getFixturePath('/simple/file2.yaml');
+  const filepath1 = getFixturePath('/plain/file1.yml');
+  const filepath2 = getFixturePath('/plain/file2.yaml');
   expect(genDiff(filepath1, filepath2)).toEqual(expectedOutput);
 });
 
 test('gendiff no extension json + yaml', () => {
-  const filepath1 = getFixturePath('/simple/file1');
-  const filepath2 = getFixturePath('/simple/file2.yaml');
+  const filepath1 = getFixturePath('/plain/file1');
+  const filepath2 = getFixturePath('/plain/file2.yaml');
   expect(genDiff(filepath1, filepath2)).toEqual(expectedOutput);
 });
 
 test('gendiff nested json', () => {
-  const filepath1 = getFixturePath('/file1.json');
-  const filepath2 = getFixturePath('/file2.json');
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  expect(genDiff(filepath1, filepath2)).toEqual(output);
+});
+
+test('gendiff nested yaml', () => {
+  const filepath1 = getFixturePath('file1.yaml');
+  const filepath2 = getFixturePath('file2.yml');
   expect(genDiff(filepath1, filepath2)).toEqual(output);
 });
--- src/index.js
+++ src/index.js
@@ -2,33 +2,60 @@ import _ from 'lodash';
 import parse from './parsers.js';
 
 export default (filepath1, filepath2) => {
-  const obj1 = parse(filepath1);
-  const obj2 = parse(filepath2);
+  const object1 = parse(filepath1);
+  const object2 = parse(filepath2);
 
-  const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
+  const diff = (obj1, obj2) => {
+    const keys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
+    return keys.flatMap((key) => {
+      const value1 = obj1[key];
+      const value2 = obj2[key];
 
-  const diff = keys.flatMap((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-    if (!_.has(obj1, key)) {
-      return [['+', [key, value2]]];
-    }
-    if (!_.has(obj2, key)) {
-      return [['-', [key, value1]]];
-    }
-    return (
-      value1 === value2
-        ? [[' ', [key, value1]]]
-        : [['-', [key, value1]], ['+', [key, value2]]]
-    );
-  });
-
-  const format = (data) => {
-    const INDENT = '  ';
-    const lines = data.map(([sign, [key, value]]) => `${INDENT}${sign} ${key}: ${value}`);
-    return ['{', ...lines, '}'].join('\n');
+      if (!_.has(obj1, key)) {
+        return { type: 'added', key, value: value2 };
+      }
+      if (!_.has(obj2, key)) {
+        return { type: 'deleted', key, value: value1 };
+      }
+      if (value1 === value2) {
+        return { type: 'unmodified', key, value: value1 };
+      }
+      if (typeof value1 === 'object') {
+        return { type: 'modified', key, value: (diff(value1, value2)) };
+      }
+      return [{ type: 'deleted', key, value: value1 }, { type: 'added', key, value: value2 }];
+    });
   };
-  const result = format(diff);
+  console.log(diff(object1, object2));
+
+  const format = (data, depth = 1) => {
+    const INDENT = '  ';
+    const currentIndent = INDENT.repeat(depth);
+    const bracketIndent = INDENT.repeat(depth - 1);
+    const lines = data.map(({ type, key, value }) => {
+      let sign = ' ';
+      switch (type) {
+        case 'added':
+          sign = '+';
+          break;
+        case 'deleted':
+          sign = '-';
+          break;
+        case 'modified':
+          sign = ' ';
+          break;
+        default:
+          sign = ' ';
+      }
+
+      if (type === 'modified') {
+        return `${currentIndent}${sign} ${key}: ${format(value, depth + 2)}`;
+      }
+      return `${currentIndent}${sign} ${key}: ${value}`;
+    });
+    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+  };
+  const result = format(diff(object1, object2));
 
   return result;
 };
