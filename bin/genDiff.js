4e25f7c9f25247e2b01ff429330ee3ace766581f
O. Marchenko <alex.mar4enko@gmail.com>
2022-01-11 01:20:52 -0800

    fix: config path resolve, default output to console

--- bin/genDiff.js
+++ bin/genDiff.js
@@ -1,22 +1,28 @@
 #!/usr/bin/env node
 import { readFileSync } from 'fs';
-import path from 'path';
+import { dirname } from 'path';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
 import { Command } from 'commander/esm.mjs';
 import genDiff from '../src/index.js';
 
-const packageConfig = JSON.parse(readFileSync(path.resolve('./package.json')));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getConfigPath = () => path.join(__dirname, '..', './package.json');
+const packageConfig = JSON.parse(readFileSync(path.resolve(getConfigPath())));
 
 const program = new Command();
 program
   .description(packageConfig.description)
   .version(packageConfig.version)
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format [stylish|plain|json]', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
     const absPath1 = path.resolve(filepath1);
     const absPath2 = path.resolve(filepath2);
     const options = program.opts();
-    genDiff(absPath1, absPath2, options.format);
+    console.log(genDiff(absPath1, absPath2, options.format));
   });
 program.parse();
