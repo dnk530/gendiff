95807ab2370cd95b4ea459bae402c9e129d42068
O. Marchenko <alex.mar4enko@gmail.com>
2022-01-14 22:25:37 -0800

    fix: encoding letter case

--- src/parsers.js
+++ src/parsers.js
@@ -4,7 +4,7 @@ import yaml from 'js-yaml';
 import _ from 'lodash';
 
 export default (filepath) => {
-  const file = readFileSync(path.resolve(filepath), 'UTF-8');
+  const file = readFileSync(path.resolve(filepath), 'utf8');
   const extension = path.extname(filepath);
   const extensionToParser = {
     '.json': JSON.parse,
