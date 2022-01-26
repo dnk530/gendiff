ba03c1cb04c09da35efc246c67966d313a2f0512
O. Marchenko <alex.mar4enko@gmail.com>
2022-01-25 16:56:36 -0800

    refactor: parser module to work with data only, file reading moved to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import trimStart from 'lodash/trimStart.js';
 import parse from './parsers.js';
 import buildAst from './astBuilder.js';
 import formatAst from './formatters/index.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = parse(filepath1);
-  const data2 = parse(filepath2);
+  const getFile = (filepath) => readFileSync(path.resolve(filepath), 'utf8');
+  const getExtension = (filepath) => trimStart(path.extname(filepath), '.').toLowerCase();
+  const getFileFormat = (filepath) => {
+    const extension = getExtension(filepath);
+    if (extension.length === 0) {
+      return 'json';
+    }
+    const extensionToFormat = {
+      json: 'json',
+      yml: 'yaml',
+      yaml: 'yaml',
+    };
+    return extensionToFormat[extension];
+  };
+  const data1 = parse(getFile(filepath1), getFileFormat(filepath1));
+  const data2 = parse(getFile(filepath2), getFileFormat(filepath2));
   const ast = buildAst(data1, data2);
   return formatAst(ast, formatName);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -1,22 +1,9 @@
-import { readFileSync } from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
-import trimStart from 'lodash/trimStart.js';
-import has from 'lodash/has.js';
 
-export default (filepath) => {
-  const file = readFileSync(path.resolve(filepath), 'utf8');
-  const extension = trimStart(path.extname(filepath), '.').toLowerCase();
-  if (extension.length === 0) {
-    return JSON.parse(file);
-  }
-  const extensionToParser = {
+export default (data, dataFormat) => {
+  const dataFormatToParser = {
     json: JSON.parse,
     yaml: yaml.load,
-    yml: yaml.load,
   };
-  if (!has(extensionToParser, extension)) {
-    throw new Error(`Unknown file format: '${extension}'`);
-  }
-  return extensionToParser[extension](file);
+  return dataFormatToParser[dataFormat](data);
 };
